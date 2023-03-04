a936195477c79d0c315f515f3b149266d8eb5a03
Marius Tudorache <mariustudorache39@gmail.com>
2023-03-04 10:38:43 +0100

    added product model

---
+++ src/main/java/eu/asaitec/fruitshop/product/model/Product.java
@@ -0,0 +1,23 @@
+package eu.asaitec.fruitshop.product.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+}
